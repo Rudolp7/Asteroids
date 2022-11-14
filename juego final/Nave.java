55fd07e9d295d284f9b07bb473458531798bdb16
Rudolp7 <112682101+Rudolp7@users.noreply.github.com>
2022-11-14 08:34:19 -0600

    Add files via upload

---
+++ juego final/Nave.java
@@ -0,0 +1,21 @@
+//Metodos:constructor,avanzar,izquierda,derecha,disparar.
+
+public Class Nave {
+    private int posX,posY; //posicion de  x,y 
+    private int desX,desY; //desplazamiento de los valores en x,y
+    private Color  color; //color que tendra la nave
+    private double speed; //velocidad de la nave 
+    private int live; //vida de la nave 
+    private double angule; //angulo que tomara la nave 
+
+
+    public Nave (){
+        this.posX=x:
+        this.posY=y;
+    }
+
+
+
+
+
+}
---
+++ juego final/Ship.java
@@ -0,0 +1,14 @@
+public Class Ship{
+    private int posX,posY;
+    private double angulo;
+    private double speed;
+    private int daño;
+    private Color color;
+
+    public Ship(){
+        this.posX=x;
+        this.posY=y;
+        
+    }
+
+}
